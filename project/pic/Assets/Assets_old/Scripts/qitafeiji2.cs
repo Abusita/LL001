2f9e24759808d09b0c3aea2ffe5f3e590fac668b
DrPeas <978337929@qq.com>
2019-10-14 17:42:55 +0800

    Revert "提交："
    This reverts commit 4876f7785f93a5ededdc8eb634098474ba52bda0.

---
+++ project/pic/Assets/Assets_old/Scripts/qitafeiji2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class qitafeiji2 : MonoBehaviour {
+    Transform m_transform;
+
+	// Use this for initialization
+	void Start () {
+        m_transform = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        StartCoroutine(shuaxinweizhi());
+    }
+
+    IEnumerator shuaxinweizhi()
+    {
+        yield return new WaitForSeconds(3);
+        m_transform.position = new Vector3(zishenzhanji.mPosx[10], zishenzhanji.mPosy[10], 0);
+    }
+}
---
+++ project/pic/Assets/Assets_old/Scripts/yidong.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yidong : MonoBehaviour {
+    Transform m_transform;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(100, 100, 100) * Time.deltaTime);
+    }
+}
